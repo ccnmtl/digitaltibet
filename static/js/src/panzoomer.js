7758f40e8c9c046ff3b3da8be3b44fbf08afa5bd
Nik Nyby <nnyby@columbia.edu>
2017-08-01 14:48:47 -0400

    Wait for img load before using width - PMT #111904

--- static/js/src/panzoomer.js
+++ static/js/src/panzoomer.js
@@ -1,22 +1,29 @@
 /* eslint-env jquery */
+/* globals loadImage */
 
 $(document).ready(function() {
+    if ($('.collection-image').length < 1) {
+        return;
+    }
+
     // Connect the panzoomer up to the user controls
     var $section = $('.collection-image').first();
     var $img = $section.find('img');
     var $pz = $('.panzoomer').first();
 
     var containerWidth = $section.width();
-    var imgWidth = $img.width();
-    var diff = (containerWidth - imgWidth) / 2;
+    loadImage($img[0].src).then(function() {
+        var imgWidth = $img.width();
+        var diff = (containerWidth - imgWidth) / 2;
 
-    $img.panzoom({
-        $zoomIn: $pz.find('.zoom-in'),
-        $zoomOut: $pz.find('.zoom-out'),
-        $zoomRange: $pz.find('.zoom-range'),
-        $reset: $pz.find('.reset'),
+        $img.panzoom({
+            $zoomIn: $pz.find('.zoom-in'),
+            $zoomOut: $pz.find('.zoom-out'),
+            $zoomRange: $pz.find('.zoom-range'),
+            $reset: $pz.find('.reset'),
 
-        // Center the image within the panzoom container
-        startTransform: 'translateX(' + diff + 'px)'
+            // Center the image within the panzoom container
+            startTransform: 'translateX(' + diff + 'px)'
+        });
     });
 });
